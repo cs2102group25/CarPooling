fd620c3165f88767ebf974fdae9b1eb7254f46c7
m133225 <m133225@hotmail.com>
2016-04-02 00:22:14 +0800

    Improve add trip table and extract header into a separate php.

---
+++ header.php
@@ -0,0 +1,16 @@
+<div class="header">
+		<div style="float:left; font-size:4vmin;"><b>Trips available</b></div>
+    <div style="float:right;">
+        <?php    
+        if (!isset($_SESSION['email'])) {
+            echo "
+          <button type='button' class='btn btn-info btn-lg' id='login-btn' onclick='location.href=\"login.php\"'>Login</button>
+          <button type='button' class='btn btn-info btn-lg' id='signup-btn' onclick='location.href=\"signup.php\"'>Sign up</button>";
+        } else {
+            echo "Logged in as ".$_SESSION['email'].".
+          <button type='button' class='btn btn-info btn-lg' id='signup-btn' onclick='location.href=\"logout.php\"'>Logout</button>";
+        }
+    ?>
+      
+    </div>
+</div>
--- index.php
+++ index.php
@@ -15,25 +15,8 @@ session_start();
 </head>
 
 <body>
-    <?php
-    ?>
-	<div class="header">
-		<div style="float:left; font-size:4vmin;"><b>Trips available</b></div>
-    <div style="float:right;">
-        <?php    
-        if (!isset($_SESSION['email'])) {
-            echo "
-          <button type='button' class='btn btn-info btn-lg' id='login-btn' onclick='location.href=\"login.php\"'>Login</button>
-          <button type='button' class='btn btn-info btn-lg' id='signup-btn' onclick='location.href=\"signup.php\"'>Sign up</button>";
-        } else {
-            echo "Logged in as ".$_SESSION['email'].".
-          <button type='button' class='btn btn-info btn-lg' id='signup-btn' onclick='location.href=\"logout.php\"'>Logout</button>";
-        }
-    ?>
-      
-    </div>
-	</div>
-
+    <?php require_once 'header.php';?>
+	
   <table class="resultTable">
     <tr>
       <td>
@@ -42,12 +25,13 @@ session_start();
                  <input type="submit" name="searchForTrip" value="Search">
         </form>
         <form method='post'>
-          <?php require_once 'php/sqlconn.php';
+        <?php 
+            require_once 'php/sqlconn.php';
           $arrayTitle = ["From", "To", "When", "Duration", "No. Seats", "By", "Price", "Plate no", "Actions"];
 
           $query;
           if (isset($_GET['searchForTrip'])) {
-            $query = 'SELECT * FROM trip WHERE pick_up LIKE \'%'.$_GET['searchQuery'].'%\' OR drop_off LIKE \'%'.$_GET['searchQuery'].'%\'';
+            $query = 'SELECT * FROM provides_trip WHERE start_loc LIKE \'%'.$_GET['searchQuery'].'%\' OR end_loc LIKE \'%'.$_GET['searchQuery'].'%\'';
           } else {
             $query = 'SELECT * FROM provides_trip';
           }
@@ -55,29 +39,28 @@ session_start();
           $result = pg_query($query) or die('Query failed: '.pg_last_error());
 
           echo "<div class='container'><div class='row'>";	
-          for($i = 0; $i < count($arrayTitle); $i++) {
-            if($i == 0 || $i == 1 || $i == 7) {
-             echo "<div class='col-lg-2 col-md-2 result'>".$arrayTitle[$i]."</div>";
-           }
-           else {
-             echo "<div class='col-lg-1 col-md-1 result'>".$arrayTitle[$i]."</div>";
-           }
+          for ($i = 0; $i < count($arrayTitle); $i++) {
+            if ($i == 0 || $i == 1 || $i == 7) {
+              echo "<div class='col-lg-2 col-md-2 result'>".$arrayTitle[$i]."</div>";
+            } else {
+              echo "<div class='col-lg-1 col-md-1 result'>".$arrayTitle[$i]."</div>";
+            }
          }
          echo "</div>";
 
-        while ($line = pg_fetch_row($result)) {
-          echo "<div class='row'>";
-          for ($i = 0; $i < count($line); $i++) {
-            if($i == 0 || $i == 1) {}
-          else if($i == 2 || $i == 3 || $i == 9) {
-            echo "<div class='col-lg-2 col-md-2 result'>".$line[$i]."</div>";	
-          } else {
-            echo "<div class='col-lg-1 col-md-1 result'>".$line[$i]."</div>";
-          }
-        }
-        echo "<div class='col-lg-1 col-md-1 result'><input type='submit' name=delete[] value='Delete'><br>
-        <input type='submit' name=update[] value='Update'></div></div>";
-      }
+         while ($line = pg_fetch_row($result)) {
+           echo "<div class='row'>";
+           for ($i = 0; $i < count($line); $i++) {
+             if ($i == 0 || $i == 1) {}
+             else if ($i == 2 || $i == 3 || $i == 9) {
+               echo "<div class='col-lg-2 col-md-2 result'>".$line[$i]."</div>";	
+             } else {
+               echo "<div class='col-lg-1 col-md-1 result'>".$line[$i]."</div>";
+             }
+           }
+           echo "<div class='col-lg-1 col-md-1 result'><input type='submit' name=delete[] value='Delete'><br/>
+           <input type='submit' name=update[] value='Update'></div></div>";
+         }
       echo "</div>";
       pg_free_result($result);
       ?>
@@ -87,32 +70,97 @@ session_start();
   <tr> <td>
     Add new trip
     <form method="post" action="index.php">
-      Plate No <input type="text" name="plate-no" placeholder="Your car plate number"><br>
-      Pick-up <input type="text" name="pick-up" placeholder="Pick-up"><br>
-      Drop-off <input type="text" name="drop-off" placeholder="Drop-off"><br>
-      Time <input type="text" name="time" value="00:00:00"><br>
-      Est. duration <input type="text" name="duration" placeholder="0 minutes"><br>
-      Seats available <input type="number" name="seat-available" value=1 min=1><br>
-      Vehicle <input type="text" name="vehicle" placeholder="Vehicle mode"><br>
-      Price <input type="text" name="price" placeholder="0"><br>
-      <input type="submit" name="add" value="Add">
+    <div class="addTrip">
+        <div class="row">
+            <div class="col-lg-4 col-md-4">
+                Vehicle:
+            </div>
+            <div class="col-lg-8 col-md-8">
+                <select>
+                    <?php
+                    $vehicleQuery = "SELECT c.car_plate, c.model FROM car c, ownership o WHERE '".$_SESSION['email']."' = o.email AND o.car_plate = c.car_plate;";
+                    $vehicleResult = pg_query($vehicleQuery)or die('Query failed: '.pg_last_error());;
+                    $numRows = pg_num_rows($vehicleResult);
+                    for ($i = 0; $i < $numRows; $i++) {
+                        $curVehicle = pg_fetch_row($vehicleResult);
+                        echo "<option value='".$curVehicle[0]."' >".$curVehicle[0]." (".$curVehicle[1].")"."</option>";
+                    }
+                    ?>
+                </select>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-4">
+                Pick-up Point
+            </div>
+            <div class="col-lg-8 col-md-8">
+                <input type="text" name="pick-up" placeholder="Pick-up" />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-4">
+                Drop-off Point
+            </div>
+            <div class="col-lg-8 col-md-8">
+                <input type="text" name="drop-off" placeholder="Drop-off"/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-4">
+                Starting time
+            </div>
+            <div class="col-lg-8 col-md-8">
+                <input type="text" name="time" value="00:00:00"/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-4">
+                Estimated Duration
+            </div>
+            <div class="col-lg-8 col-md-8">
+                <input type="text" name="duration" placeholder="0 minutes"/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-4">
+                Seats available
+            </div>
+            <div class="col-lg-8 col-md-8">
+                <input type="number" name="seats-available" value=1 min=1 />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-4">
+                Price
+            </div>
+            <div class="col-lg-8 col-md-8">
+                <input type="text" name="price" placeholder="0" />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <input type="submit" name="add" value="Add" />
+            </div>
+        </div>
+    </div>
     </form>
+      </td>
+      </tr>
 
     <?php require_once 'php/sqlconn.php';
         require_once 'libs.php';
 
-        if(!isset($_SESSION['email'], $_SESSION['password'])) {
+        if (!isset($_SESSION['email'], $_SESSION['password'])) {
           exit;
         }
 
         // Add Functions
-        if(isset($_POST['add'], $_POST['pick-up'], $_POST['drop-off'], $_POST['time'], $_POST['duration'], $_POST['seat-available'],
+        if (isset($_POST['add'], $_POST['pick-up'], $_POST['drop-off'], $_POST['time'], $_POST['duration'], $_POST['seat-available'],
           $_POST['vehicle'], $_POST['price'], $_POST['plate-no'])){
 
-          $num_rows = countingRows("trip");
-          $query = "INSERT INTO trip(trip_id, email, pick_up, drop_off, time, duration, seat_available, vehicle, price, plate_no)
-          VALUES(".($num_rows + 1).", '".$_SESSION['user_name']."', '".$_POST['pick-up']."', '".$_POST['drop-off']."', '".$_POST['time']."', '"
-          .$_POST['duration']."', ".$_POST['seat-available'].", '".$_POST['vehicle']."', ".$_POST['price'].", '".$_POST['plate-no']."')";
+          //for (int i = 0; i < $_POST['seat_no'])
+          //$query = "INSERT INTO provides_trip(seat_no, car_plate, price, start_time, end_time, start_loc, end_loc, posted)
+          //VALUES($seats_available, )";
 
           $result = pg_query($query) or die('Query failed: '.pg_last_error());
           
@@ -122,7 +170,7 @@ session_start();
             echo "Trip not added";
           }
         } else {
-          echo "Missing input<br>";
+          echo "Missing input<br/>";
         }
 
         // Delete + Update functions

0eb61d8269d03ed3bd9f7441312a4b623df70dcb
m133225 <m133225@hotmail.com>
2016-04-01 19:23:38 +0800

    Implemented logout, and hiding of signup/login buttons when logged in.

--- index.php
+++ index.php
@@ -1,16 +1,36 @@
 <!DOCTYPE html>
+<?php
+session_start();
+?>
 <html>
 <head>
 	<title>CS2102 Car Pooling</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+    <script src="http://code.jquery.com/jquery-2.2.0.min.js"></script>
+  <script src="js/js.js" type="text/javascript"></script>
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </head>
 
 <body>
+    <?php
+    ?>
 	<div class="header">
 		<div style="float:left; font-size:4vmin;"><b>Trips available</b></div>
     <div style="float:right;">
-      <button type="button" class="btn btn-info btn-lg" id="login-btn" onclick="location.href='login.php'">Login</button>
-      <button type="button" class="btn btn-info btn-lg" id="signup-btn" onclick="location.href='signup.php'">Sign up</button>
+        <?php    
+        if (!isset($_SESSION['email'])) {
+            echo "
+          <button type='button' class='btn btn-info btn-lg' id='login-btn' onclick='location.href=\"login.php\"'>Login</button>
+          <button type='button' class='btn btn-info btn-lg' id='signup-btn' onclick='location.href=\"signup.php\"'>Sign up</button>";
+        } else {
+            echo "Logged in as ".$_SESSION['email'].".
+          <button type='button' class='btn btn-info btn-lg' id='signup-btn' onclick='location.href=\"logout.php\"'>Logout</button>";
+        }
+    ?>
+      
     </div>
 	</div>
 
@@ -29,7 +49,7 @@
           if (isset($_GET['searchForTrip'])) {
             $query = 'SELECT * FROM trip WHERE pick_up LIKE \'%'.$_GET['searchQuery'].'%\' OR drop_off LIKE \'%'.$_GET['searchQuery'].'%\'';
           } else {
-            $query = 'SELECT * FROM trip';
+            $query = 'SELECT * FROM provides_trip';
           }
 
           $result = pg_query($query) or die('Query failed: '.pg_last_error());
@@ -133,11 +153,6 @@
 
   <footer class="footer"> Copyright &#169; CS2102</footer>
 
-  <script src="http://code.jquery.com/jquery-2.2.0.min.js"></script>
-  <script src="js/js.js" type="text/javascript"></script>
   
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </body>
 </html>

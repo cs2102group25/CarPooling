b18e51de1a7be22640952aa85692bea88fa58882
m133225 <m133225@hotmail.com>
2016-04-02 11:08:03 +0800

    Fixed booking table relation.

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ session_start();
         if (isset($_POST['pick-up'], $_POST['drop-off'], $_POST['time'], $_POST['duration'], $_POST['seats'],
          $_POST['price'], $_POST['vehicle'])) {
           for ($i = 0; $i < $_POST['seats']; $i++) {
-            $endTime = date('Y-m-d h:i:s', strtotime("+".$_POST['duration']." minutes", strtotime($_POST['time'])));
+            $endTime = date('Y-m-d h:i:s', strtotime("+".$_POST['duration'], strtotime($_POST['time'])));
             $addQuery = "INSERT INTO provides_trip(seat_no, car_plate, price, start_time, end_time, start_loc, end_loc, posted)
             VALUES(".($i+1).", '".$_POST['vehicle']."', '".$_POST['price']."', '".$_POST['time']."', '$endTime', '".$_POST['pick-up']."', '".$_POST['drop-off']."', 'true')";
 
@@ -57,7 +57,7 @@ session_start();
         <form method='post'>
         <?php 
           require_once 'php/sqlconn.php';
-          $arrayTitle = ["From", "To", "When", "Duration", "Seat No.", "Price", "Vehicle", "Actions"];
+          $arrayTitle = ["From", "To", "Start Time", "End Time", "Seat No.", "Price", "Vehicle", "Actions"];
           if (isset($_GET['searchForTrip'])) {
             $query = 'SELECT * FROM provides_trip WHERE start_loc LIKE \'%'.$_GET['searchQuery'].'%\' OR end_loc LIKE \'%'.$_GET['searchQuery'].'%\'';
           } else {
@@ -68,7 +68,7 @@ session_start();
 
           echo "<div class='container'><div class='row'>";	
           for ($i = 0; $i < count($arrayTitle); $i++) {
-            if ($i == 0 || $i == 1 || $i == 2 || $i == 6) {
+            if ($i == 0 || $i == 1 || $i == 2 || $i == 3) {
               echo "<div class='col-lg-2 col-md-2 result'>".$arrayTitle[$i]."</div>";
             } else {
               echo "<div class='col-lg-1 col-md-1 result'>".$arrayTitle[$i]."</div>";
@@ -82,13 +82,13 @@ session_start();
            echo "<div class='col-lg-2 col-md-2 result'>".$line[6]."</div>";	
 
            echo "<div class='col-lg-2 col-md-2 result'>".$line[3]."</div>";
-           echo "<div class='col-lg-1 col-md-1 result'>".$line[4]."</div>";
+           echo "<div class='col-lg-2 col-md-2 result'>".$line[4]."</div>";
 
            echo "<div class='col-lg-1 col-md-1 result'>".$line[0]."</div>";
 
            echo "<div class='col-lg-1 col-md-1 result'>".$line[2]."</div>";
 
-           echo "<div class='col-lg-2 col-md-2 result'>".$line[1]."</div>";
+           echo "<div class='col-lg-1 col-md-1 result'>".$line[1]."</div>";
            echo "<div class='col-lg-1 col-md-1 result'><button type='submit' name=delete value='".$line[1]."_".$line[0]."'] >Delete</button></div>";
            echo "</div>";
          }

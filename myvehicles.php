02a31e4af85b079d3129eb9b1d3ba4fc096cfe51
Le Cong Thang <lecongthang.1102@gmail.com>
2016-04-03 13:11:53 +0800

    Delete function in vehicle

--- myvehicles.php
+++ myvehicles.php
@@ -1,6 +1,6 @@
-
 <!DOCTYPE html>
 <html>
+<body>
 <?php
 session_start();
 $title = "My Vehicles";
@@ -11,93 +11,114 @@ require_once 'menu.php';
 if (!isset($_SESSION['email'])) {
     directToLoginPage();
 }
-    
+
 if (isset($_POST['addVehicle'])) {
     $addCarQuery = "INSERT INTO car (car_plate, model) VALUES('".$_POST['car_plate']."', '".$_POST['model']."');";
     $addCarResult = pg_query($addCarQuery);
     
     $addOwnershipQuery = "INSERT INTO ownership (email, car_plate, expiration) VALUES('".$_SESSION['email']."', '".$_POST['car_plate']."', '".$_POST['expiration']."');";
     $addOwnershipResult = pg_query($addOwnershipQuery);
+    if(!$addOwnershipResult || !$addCarResult) exit("There's error in adding car result");
+}
+
+if(isset($_POST['deleteVehicle'])) {
+    $delOwnershipQuery = "DELETE FROM Ownership WHERE car_plate='".$_POST['deleteVehicle']."'";
+    $delOwnershipResult = pg_query($delOwnershipQuery);
+
+    $delBookingQuery = "DELETE FROM Booking WHERE car_plate='".$_POST['deleteVehicle']."'";
+    $delBookingResult = pg_query($delBookingQuery);
+
+    $delTripQuery = "DELETE FROM Provides_Trip WHERE car_plate='".$_POST['deleteVehicle']."'";
+    $delTripResult = pg_query($delTripQuery);
+
+    $delCarQuery = "DELETE FROM Car WHERE car_plate='".$_POST['deleteVehicle']."'";
+    $delCarResult = pg_query($delCarQuery);
+
+    if(!$delOwnershipResult || !$delBookingResult || !$delTripResult || !$delCarResult) exit("There's error in deleting vehicles");
 }
 ?>
     <table class="resultTable">
-    <tr>
-        <td>
-    <?php
-echo "<div class='container'>";
-$vehicleQuery = "SELECT c.car_plate, c.model FROM car c, ownership o WHERE c.car_plate = o.car_plate AND o.email = '".$_SESSION['email']."';";
+        <tr>
+            <td>
+                <?php
+                echo "<div class='container'>";
+                $vehicleQuery = "SELECT c.car_plate, c.model FROM car c, ownership o WHERE c.car_plate = o.car_plate AND o.email = '".$_SESSION['email']."';";
 
-$arrayTitle = ['Plate No.', 'Model'];
-echo "<div class='row result'>";
+                $arrayTitle = ['Plate No.', 'Model', 'Delete'];
+                echo "<div class='row result'>";
 
-echo "<div class='col-md-6'>".$arrayTitle[0]."</div>";
-echo "<div class='col-md-6'>".$arrayTitle[1]."</div>";
+                echo "<div class='col-md-5'>".$arrayTitle[0]."</div>";
+                echo "<div class='col-md-5'>".$arrayTitle[1]."</div>";
+                echo "<div class='col-md-2'>".$arrayTitle[2]."</div>";
 
-echo "</div>";
+                echo "</div>";
 
-$vehicleResult = pg_query($vehicleQuery);
+                $vehicleResult = pg_query($vehicleQuery);
+                if(!$vehicleResult) exit("There's error in displaying vehicle results");
 
-if ($vehicleResult && ($vehicleCount = pg_num_rows($vehicleResult)) > 0) {
-    for ($i = 0; $i < $vehicleCount; $i++) {
-        $row = pg_fetch_row($vehicleResult);
+                if (($vehicleCount = pg_num_rows($vehicleResult)) > 0) {
+                    for ($i = 0; $i < $vehicleCount; $i++) {
+                        $row = pg_fetch_row($vehicleResult);
 
-        echo "<div class='row result'>";
-            echo "<div class='col-md-6'>".$row[0]."</div>";
-            echo "<div class='col-md-6'>".$row[1]."</div>";
-        echo "</div>";
-    }
-} else {
-    echo "<div class='row result'>";
-        echo "<div class='col-md-12'>You have no vehicles.</div>";
-    echo "</div>";
-}
-    
+                        echo "<div class='row result'>";
+                        echo "<div class='col-md-5'>".$row[0]."</div>";
+                        echo "<div class='col-md-5'>".$row[1]."</div>";
+                        echo "<div class='col-md-2'><form method='post' action='myvehicles.php'>
+                        <button type='submit' name='deleteVehicle' value='".$row[0]."'>Delete</button></form></div>";
+                        echo "</div>";
+                    }
+                } else {
+                    echo "<div class='row result'>";
+                    echo "<div class='col-md-12'>You have no vehicles.</div>";
+                    echo "</div>";
+                }
 
-echo "</div>";
-?>
-        </td>
+                echo "</div>";
+                ?>
+            </td>
         </tr>
         <tr>
             <td>
-Add new vehicle
-    <form method="post" action="myvehicles.php">
-    <div class="addInfo">
-        <div class="row">
-            <div class="col-lg-4 col-md-4">
-                Car Plate No.
-            </div>
-            <div class="col-lg-8 col-md-8">
-                <input type="text" name="car_plate" placeholder="Car plate number" />
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-4 col-md-4">
-                Model
-            </div>
-            <div class="col-lg-8 col-md-8">
-                <input type="text" name="model" placeholder="Model"/>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-4 col-md-4">
-                Ownership Expiration
-            </div>
-            <div class="col-lg-8 col-md-8">
-                <input id="datepicker" type="text" name="expiration" />
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12 col-md-12">
-                <input type="submit" name="addVehicle" value="Add vehicle" />
-            </div>
-        </div>
-    </div>
-    </form>
+                Add new vehicle
+                <form method="post" action="myvehicles.php">
+                    <div class="addInfo">
+                        <div class="row">
+                            <div class="col-lg-4 col-md-4">
+                                Car Plate No.
+                            </div>
+                            <div class="col-lg-8 col-md-8">
+                                <input type="text" name="car_plate" placeholder="Car plate number" />
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-4 col-md-4">
+                                Model
+                            </div>
+                            <div class="col-lg-8 col-md-8">
+                                <input type="text" name="model" placeholder="Model"/>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-4 col-md-4">
+                                Ownership Expiration
+                            </div>
+                            <div class="col-lg-8 col-md-8">
+                                <input id="datepicker" type="text" name="expiration" />
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12">
+                                <input type="submit" name="addVehicle" value="Add vehicle" />
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </td>
         </tr>
     </table>
-    <script>
-        $('#datepicker').datepicker();
-    </script>
     <?php require_once 'footer.php'; ?>
+    <script>
+    $('#datepicker').datepicker();
+    </script>
+</body>
 </html>
